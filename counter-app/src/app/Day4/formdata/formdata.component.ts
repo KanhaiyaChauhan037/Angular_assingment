06dbda1daaeaa31e60df6a7aa02ec9a03403d865
KanhaiyaChauhan037 <kanhaiyachauhan047@gmail.com>
2023-10-13 23:01:02 +0530

    day-4

--- counter-app/src/app/Day4/formdata/formdata.component.ts
+++ counter-app/src/app/Day4/formdata/formdata.component.ts
@@ -7,4 +7,41 @@ import { Component } from '@angular/core';
 })
 export class FormdataComponent {
 
+  // data: any=[];
+  // formSubmitted = false;
+  // postdata(data: any) {
+
+    
+  // this.data = data
+  //     this.formSubmitted = true;
+  // }
+  data: any[] = [];
+
+
+  dataModel: any = {
+    name: '',
+    desc: '',
+    price: 0
+  };
+
+
+  formSubmitted = false;
+
+
+  postdata(formData: any) {
+    if (formData) {
+      const newData = { ...this.dataModel };
+
+      newData.name = formData.name;
+      newData.desc = formData.desc;
+      newData.price = formData.price;
+
+      this.data.push(newData);
+     
+// console.log("dat",this.data)
+      this.formSubmitted = true;
+    }
+
+  }
+
 }
